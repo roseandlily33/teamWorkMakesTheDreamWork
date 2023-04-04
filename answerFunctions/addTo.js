3408ef7edb6a1b41c11f0fbbdfad968ba74a06c6
Victoria Benoit <vrose834@gmail.com>
2023-04-04 18:10:25 -0300

    Got the addTo working

--- answerFunctions/addTo.js
+++ answerFunctions/addTo.js
@@ -13,14 +13,14 @@ const addEmployee = () => {
             type: 'input',
             message: 'Employees Last Name?',
             name: 'emLast',
-           
+
         },
         {
             type: 'list',
             message: 'What is the employees role?',
             name: 'emRole',
             choices: [1, 2, 3, 4],
-        }, 
+        },
         {
             type: 'list',
             message: 'Who is the employees manager?',
@@ -29,13 +29,15 @@ const addEmployee = () => {
         }
 
     ])
-    .then( answers => {
-    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)', answers.emName, answers.emLast, answers.emRole, answers.emManager, function(err, results) {
-        if(err){console.log(err)} else {
-            console.table(results);
-        }
-    })})
-    .catch(err => console.error(err));
+        .then(answers => {
+            db.query('INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)', [answers.emName, answers.emLast, answers.emRole, answers.emManager], function (err, results) {
+                if (err) { console.log(err) }
+                else {
+                    console.table(results);
+                }
+            })
+        })
+        .catch(err => console.error(err));
 }
 const addRole = () => {
     inquirer.prompt([
@@ -43,43 +45,49 @@ const addRole = () => {
             type: 'input',
             message: 'What is the role?',
             name: 'roleName',
-            
+
         },
         {
             type: 'input',
             message: 'What is the salary?',
             name: 'roleSalary',
-            
-         },
+
+        },
         {
             type: 'list',
             message: 'What department does this role go in?',
             name: 'roleDept',
-            choices: ['Sales', 'Engineering', 'Finance', 'Legal'],
+            choices: [1, 2, 3, 4],
         },
     ])
-    .then(answers => {
-    db.query('INSERT INTO roles (title, salary, department_id) VALUES(?,?,?)', [answers.roleName, answers.roleSalary, answers.roleDept], function(err, results) {
-     
-    })})
-    .catch(err => console.error(err));
-   
+        .then(answers => {
+            db.query('INSERT INTO roles (title, salary, department_id) VALUES(?,?,?)', [answers.roleName, answers.roleSalary, answers.roleDept], function (err, results) {
+                if (err) { console.log(err) }
+                else {
+                    console.table(results);
+                }
+            })
+        })
+        .catch(err => console.error(err));
 }
 
 const addDepartment = () => {
     inquirer.prompt([
         {
-            type: 'name',
+            type: 'input',
             message: 'What is the department called?',
             name: 'deptName',
         }
     ])
-    .then(answers =>{
-    db.query('INSERT INTO departments (first_name, last_name, role_id, manager_id) VALUES(?)', answers.deptName, function(err, results){
-
-    })})
-    .catch(err => console.error(err));
+        .then(answers => {
+            db.query('INSERT INTO department (dept_name) VALUES(?)', [answers.deptName], function (err, results) {
+                if (err) { console.log(err) }
+                else {
+                    console.table(results);
+                }
+            })
+        })
+        .catch(err => console.error(err));
 }
-
 
 module.exports = { addDepartment, addEmployee, addRole };
