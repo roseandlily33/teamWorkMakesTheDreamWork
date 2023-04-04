a50f8aee658f5d73986862a61e842467a9cc7b5c
Victoria Benoit <vrose834@gmail.com>
2023-04-04 12:46:07 -0300

    Fixed the index.js

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = [
+const question = [
     {
         type: 'list',
         message: 'What would you like to do?',
         type: 'action',
         value: ['View All Employees', 'View All Roles', 'View All Departments', 'Add Employee', 'Add Role', 'Add Department', 'Update And Employee']
-    }, {
-        type: 'input',
-        message: 'Employee ID',
-        type: 'id',
-    },
-    {
-        type: 'input',
-        message: 'Email Address',
-        type: 'email',
-    },
-    {
-        type: 'input',
-        message: 'Office Number',
-        type: 'office',
-    },
-    {
-        type: 'list',
-        message: 'Select your role',
-        type: 'role',
-        choices: ['Engineer'],
-    },{
-        type: 'list',
-        message: 'Select your role',
-        type: 'role',
-        choices: ['Engineer', 'Intern', 'Finished Building'],
-    },
+    }
 ];
 //Intern: name, ID, email and school then taken back to menu
 //Engineer: name, ID, email, github username, taken back to menu
 //Finished building: HTML is generated
 function init(){
-inquirer.prompt(questions)
+inquirer.prompt(question)
 .then(response => {
     console.log(response)
 })
@@ -46,58 +21,4 @@ inquirer.prompt(questions)
     console.log(err);
 })}
 
-class Employee{
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName(){
-        this.name = name;
-    }
-    getId(){
-        this.id = id;
-    }
-    getEmail(){
-        this.email = email;
-    }
-    getRole(){
-        return 'Employee';
-    }
-}
-class Manager extends Employee{
-    constructor(name, id, email, officeNumber){
-        super(name, id, email);
-        this.officeNumber = officeNumber;
-    }
-    //get office number?
-    getRole(){
-        return 'Manager';
-    }
-}
-class Engineer extends Employee{
-    constructor(name, id, email, github){
-        super(name, id, email);
-        this.github = github;
-    }
-    getGithub(){
-        //return or no return
-        return this.github = github;
-    }
-    getRole(){
-        return 'Engineer';
-    }
-}
-class Intern extends Employee {
-    constructor(name, id, email, school){
-        super(name, id, email);
-        this.school = school;
-    }
-    getSchool(){
-        //return?
-        return this.school = school;
-    }
-    getRole(){
-        return 'Intern';
-    }
-}
+init();
