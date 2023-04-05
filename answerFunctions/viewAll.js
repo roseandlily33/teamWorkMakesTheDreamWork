9464cb2a827d6358f24d52e0ec912b849b4e071a
Victoria Benoit <vrose834@gmail.com>
2023-04-05 10:02:46 -0300

    push

--- answerFunctions/viewAll.js
+++ answerFunctions/viewAll.js
@@ -15,11 +15,10 @@ const viewAllDepartments = () => {
 }
 
 const viewAllEmployees = () => {
-    db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.dept_name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-    FROM employee
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.dept_name, roles.salary FROM employee
     LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN roles ON (roles.id = employee.role_id)
-    INNER JOIN department ON (department.id = roles.department_id)
+    INNER JOIN roles ON roles.id = employee.role_id
+    INNER JOIN department ON department.id = roles.department_id
     ORDER BY employee.id;`, function (err, results) {
         if (err) { console.log(err) }
         else {
